415b0b67348d8bea5fc4eb7f5b4997d9f9031793
Ale <aleee.eb@gmail.com>
2021-06-20 20:13:30 -0300

    Innital commit

---
+++ src/test/java/com/pantera/apiitems/ApiitemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pantera.apiitems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiitemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
